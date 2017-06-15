c18bbfc42299be9fc2914104a23f86d30424ec4a
Marko Kralj <gmarkokralj@gmail.com>
2017-06-15 20:18:48 +0200

    knex config in connections.js

--- src/server/db/connections.js
+++ src/server/db/connections.js
@@ -1,3 +1,14 @@
 const environment = process.env.NODE_ENV;
 const config = require('../../../knexfile')[environment];
-module.exports = require('knex')(config);
+module.exports = require('knex')({
+    client: 'postgresql',
+    connection: {
+      host : '127.0.0.1',
+      user : 'petar',
+      password : 'admin',
+      database : 'user_auth'
+    },
+    migrations: {
+      directory: __dirname + '/src/server/db/migrations'
+    }
+  });
